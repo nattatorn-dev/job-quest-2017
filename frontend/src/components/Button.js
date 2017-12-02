e5366b8d83abddec9cec418ed0ed35dccf448c07
nattatorn.dev <nattatorn.dev@gmail.com>
2017-12-02 19:13:11 +0700

    Updates

--- frontend/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-const Button = ({ onClick }) => {
-  return (
-    <input type="button" value="Add to cart" onClick={onClick} />
-  )
-}
-
-export default Button;
--- frontend/src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import cart from "../reducers/cart";
-import catalog from "../reducers/catalog";
-
-export default combineReducers({
-  cart,
-  catalog
-});
